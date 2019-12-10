5f870fc6e36b0ad719e3a1de41ba252b8a669618
DebaPrasad14 <mdebaprasad14@gmail.com>
2019-12-11 03:10:00 +0530

    Added logic/code to upload files

--- excel_upload/myApp/views.py
+++ excel_upload/myApp/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
-
+from django.core.files.storage import FileSystemStorage
 
 class Home(TemplateView):
 	template_name = 'home.html'
 
 
 def file_upload(request):
-    return render(request, 'file_upload.html')
+	if request.method == 'POST' and request.FILES['document']:
+		myfile = request.FILES['document']
+		fs = FileSystemStorage()
+		filename = fs.save(myfile.name, myfile)
+		url = fs.url(filename)
+		return render(request, 'file_upload.html', {'url': url} )
+	return render(request, 'file_upload.html')
