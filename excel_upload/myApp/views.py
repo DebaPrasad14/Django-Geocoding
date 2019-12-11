4974cf6e6af216594a8dcf2e6e06acf52f796de7
DebaPrasad14 <mdebaprasad14@gmail.com>
2019-12-11 22:05:55 +0530

    Added function to call google api to get latitude, longitude

--- excel_upload/myApp/views.py
+++ excel_upload/myApp/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from django.core.files.storage import FileSystemStorage
 from django.contrib import messages
+import openpyxl
+from django.conf import settings
+import pandas as pd
+import googlemaps
 
+media_root = settings.MEDIA_ROOT
+gmap_key = googlemaps.Client(key='AIzaSyBkagw73GosQC4XYPlgpjz-bWvB6VShqL4')
 
 class Home(TemplateView):
 	template_name = 'home.html'
@@ -19,7 +25,26 @@ def file_upload(request):
 			else:
 				fs = FileSystemStorage()
 				filename = fs.save(myfile.name, myfile)
-				url = fs.url(filename)
+				full_file_path=media_root+'\\'+ filename
+				get_geocode_result(full_file_path)
+
 				messages.success(request, ('Congrats !!! File uploaded successfully!'))
-				return render(request, 'file_upload.html', {'url': url} )
+				return render(request, 'file_upload.html' )
 	return render(request, 'file_upload.html')
+
+
+
+def get_geocode_result(full_file_path):
+	df = pd.read_excel(full_file_path)
+	df['LAT'] = None
+	df['LON'] = None
+	for i in range(0, len(df), 1):
+		gcode_result = gmap_key.geocode(df.iat[i,0])
+		try:
+			lat = gcode_result[0]['geometry']['location']['lat']
+			lon = gcode_result[0]['geometry']['location']['lng']
+			df.iat[i, df.columns.get_loc('LAT')] = lat
+			df.iat[i, df.columns.get_loc('LON')] = lon
+		except:
+			lat = None
+			lon = None
