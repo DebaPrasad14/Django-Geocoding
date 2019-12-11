bacbfbecdc7bbb200a52ac0e626a9bf3a0e57cf6
DebaPrasad14 <mdebaprasad14@gmail.com>
2019-12-11 16:48:52 +0530

    Added condition to check file type

--- excel_upload/myApp/views.py
+++ excel_upload/myApp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from django.core.files.storage import FileSystemStorage
+from django.contrib import messages
+
 
 class Home(TemplateView):
 	template_name = 'home.html'
@@ -9,8 +11,13 @@ class Home(TemplateView):
 def file_upload(request):
 	if request.method == 'POST' and request.FILES['document']:
 		myfile = request.FILES['document']
-		fs = FileSystemStorage()
-		filename = fs.save(myfile.name, myfile)
-		url = fs.url(filename)
-		return render(request, 'file_upload.html', {'url': url} )
+
+		if not myfile.name.endswith('.xls') and not myfile.name.endswith('.xlsx'):
+			messages.error(request, ('Please upload only excel file!'))
+		else:
+			fs = FileSystemStorage()
+			filename = fs.save(myfile.name, myfile)
+			url = fs.url(filename)
+			messages.success(request, ('Congrats !!! File uploaded successfully!'))
+			return render(request, 'file_upload.html', {'url': url} )
 	return render(request, 'file_upload.html')
